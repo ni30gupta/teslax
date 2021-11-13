cea9b2c8fa91d285517fb911770e2d74a54efd6d
ni30gupta <nguptani30@gmail.com>
2021-11-13 09:45:58 +0530

    initialize

---
+++ src/component/Top.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import "./Top.css";
+
+function Top() {
+  return (
+    <div>
+      <div>
+        <img className="img1" src="images/MX-Hero-Desktop.jpg" alt="1" />
+      </div>
+      <div>
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default Top;
